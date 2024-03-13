c273897762e0accf4a94b34cf1129824f5eb61b0
Yuan, Jinjie <jinjie.yuan@intel.com>
2024-03-13 14:57:48 +0800

    Remove useless params.
    Signed-off-by: Yuan, Jinjie <jinjie.yuan@intel.com>

--- Shears/example_commonsense.py
+++ Shears/example_commonsense.py
@@ -5,7 +5,6 @@ import torch
 from peft import PeftModel
 from transformers import AutoModelForCausalLM
 from transformers import AutoTokenizer
-from transformers import GenerationConfig
 
 
 def generate_prompt(instruction):
@@ -56,20 +55,14 @@ def main():
         prompt = generate_prompt(instruction)
         inputs = tokenizer(prompt, return_tensors="pt")
         input_ids = inputs["input_ids"].to(model.device)
-        generation_config = GenerationConfig(
-            temperature=0.1,
-            top_p=0.75,
-            top_k=40,
-            num_beams=4,
-        )
         with torch.no_grad():
             generation_output = model.generate(
                 input_ids=input_ids,
-                generation_config=generation_config,
                 return_dict_in_generate=True,
                 output_scores=True,
                 max_new_tokens=256,
                 use_cache=True,
+                num_beams=4,
             )
         s = generation_output.sequences[0]
         output = tokenizer.decode(s)
