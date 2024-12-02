af4c1c3d47862bd209be1f61888d923b1c0e4a99
Yuan Jinjie <jinjie.yuan@intel.com>
2024-12-03 03:24:21 +0800

    Fix/improve LoNAS and Shears (#5)
    Co-authored-by: J. Pablo Muñoz <pablo.munoz@intel.com>
    Signed-off-by: Yuan0320 <jinjie.yuan@intel.com>
    
    * Add warning note about outdated Shears and recommend SQFT
    
    Signed-off-by: Yuan0320 <jinjie.yuan@intel.com>
    
    ---------
    
    Signed-off-by: Yuan0320 <jinjie.yuan@intel.com>

--- Shears/install.sh
+++ Shears/install.sh
@@ -3,20 +3,19 @@ set -e
 set -x
 
 SHEARS_PATH=$PWD
-
 mkdir third_party && cd third_party
 
 # transformers
 git clone https://github.com/huggingface/transformers.git
-cd transformers && git checkout v4.31.0 && git apply --ignore-space-change --ignore-whitespace $SHEARS_PATH/patches/transformers-modifications-for-shears-usage.patch && pip install -e . && cd ..
+cd transformers && git checkout v4.31.0 && git apply --ignore-space-change --ignore-whitespace $SHEARS_PATH/patches/transformers-v4.31.0.patch && pip install -e . && cd ..
 
 # peft
 git clone https://github.com/huggingface/peft.git
-cd peft && git checkout v0.5.0 && git apply --ignore-space-change --ignore-whitespace $SHEARS_PATH/patches/peft-modifications-for-shears-usage.patch && git apply --ignore-space-change --ignore-whitespace $SHEARS_PATH/patches/peft-modifications-for-shears-inference-usage.patch && pip install -e . && cd ..
+cd peft && git checkout v0.5.0 && git apply --ignore-space-change --ignore-whitespace $SHEARS_PATH/patches/peft-v0.5.0.patch && git apply --ignore-space-change --ignore-whitespace $SHEARS_PATH/patches/peft-v0.5.0-inference.patch && pip install -e . && cd ..
 
 # nncf
 git clone https://github.com/openvinotoolkit/nncf.git
-cd nncf && git checkout 544d5141 && git apply --ignore-space-change --ignore-whitespace $SHEARS_PATH/patches/nncf-modifications-for-shears-usage.patch && pip install -e . && cd ..
+cd nncf && git checkout 544d5141 && git apply --ignore-space-change --ignore-whitespace $SHEARS_PATH/patches/nncf-544d5141.patch && pip install -e . && cd ..
 
 # others
 pip install datasets accelerate sentencepiece protobuf
